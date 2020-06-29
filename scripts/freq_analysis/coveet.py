dc4dd3eb0e61425f0b8f3c08114c6c8c218c5e99
Jerry Bonnell <j.bonnell@miami.edu>
2020-06-29 03:09:41 -0400

    add unique word function; some coveet code cleaning

--- scripts/freq_analysis/coveet.py
+++ scripts/freq_analysis/coveet.py
@@ -1,8 +1,9 @@
 import argparse
-from datetime import datetime, timedelta
 import requests
 import string
 import pandas as pd
+from datetime import datetime, timedelta
+from functools import reduce
 from itertools import combinations
 from collections import defaultdict, Counter
 from nltk.corpus import stopwords
@@ -100,71 +101,86 @@ def query_url(lang, geo, start_date, end_date=None):
     return base_url + start_date_str
 
 def handle_nlp(args):
-    df = pd.read_csv(args.file)
+    df = pd.read_csv(args.file, index_col=0)
     # convert dates encoded as strings to formal datetime object
     df['date'] = pd.to_datetime(df['date'])
+    df['text'] = df['text'].str.split()
+    df['hashtags'] = df['hashtags'].str.split()
     all_metrics = [args.ngram, args.hashtags, args.users]
     # assuming only one possible value for metric currently
     metrics = [m for m in all_metrics if m is not False]
     if len(metrics) > 1:
         raise ValueError("only one metric supported at a time")
     if isinstance(metrics[0], int):
-        freq_df = data2freq(df, metrics[0], args.top)
+        col_name = 'text'
     elif metrics[0] == 'h':
-        freq_df = data2hashtags(df, args.top)
+        col_name = 'hashtags'
+        metrics[0] = 1
     elif metrics[0] == 'u':
+        col_name = 'users'
         raise ValueError("users not supported currently")
-    print(freq_df)
+
+    df = df.dropna(subset=[col_name])
+    if args.mutex:
+        vocab_dic = uniq_vocab_by_gl(df, col_name)
+        df[col_name] = df.apply(
+            lambda x : set(x[col_name]) & vocab_dic[(x['geo'], x['lang'])],
+            result_type='reduce', axis=1)
+
+    freq_df = data2freq(df, metrics[0], args.top, text_col_name=col_name)
+
     new_fname = args.file[:args.file.rfind('.')] + f"_{metrics[0]}.csv"
+    print(freq_df.head())
     print(f"wrote freq df to {new_fname}!")
     freq_df.to_csv(new_fname)
 
-def data2hashtags(df, top_n):
-    # TODO see if we can recycle freq code
-    group = df.groupby(['date'])['hashtags']
-    # list of all the top words for every day
-    counts = []
-    dates = []
-
-    for n, g in group:
-        dates.append(n)
-        d = defaultdict(int)
-        for hashtags in g:
-            # skip empty hashtags
-            if hashtags == hashtags:
-                tags_list = "".join(hashtags.replace(' ', '')).split("#")[1:]
-                # delete any empty hashtags
-                tags_list = [tag for tag in tags_list if tag != '']
-                for tag in tags_list:
-                   d[tag] += 1
-        counts.append(dict(Counter(d).most_common(top_n)))
-    df_dic = {k:[] for top in counts for k in top.keys()}
-    for k in df_dic.keys():
-        for top in counts:
-            df_dic[k].append(0)
-            if k in top:
-                df_dic[k][-1] += top[k]
-    df_dic['date'] = dates
-    # prepare the new df
-    return pd.DataFrame(df_dic)
-
 
 def count_ngrams(df, ngram, col_name=None, count_dic=None):
-    # TODO generalize to chosen column
+    # assumes the column identified by col_name is a tokenized list of words
     if count_dic is None:
         count_dic = defaultdict(int)
 
     itr = df if col_name is None else df[col_name]
     for text in itr:
         if text == text:
-            for comb in combinations(set(text.split()), ngram):
+            for comb in combinations(set(text), ngram):
                 count_dic[tuple(sorted(comb))] += 1
 
     return count_dic
 
 
-def data2freq(df, ngram, top_n):
-    group = df.groupby(['date'])['text']
+def uniq_vocab_by_gl(df, text_col_name='text'):
+    group = df.groupby(["geo", "lang"])[text_col_name]
+    # geo, lang pair as a key that maps to the corresponding pair's vocabulary
+    # as a set
+    gl_vocab_dic = {
+        gl: [set(row) for row in g if row == row] for gl, g in group}
+    # collapse tweet units into one set for each geo-lang pair
+    for k, v in gl_vocab_dic.items():
+        vocab = set()
+        for s in v:
+            vocab.update(s)
+        gl_vocab_dic[k] = vocab
+
+    # universe of words
+    # TODO if there is a slowdown, it might be the reduce
+    all_words = reduce(set.union, [v for v in gl_vocab_dic.values()])
+    complement_vocab_dic = {k: all_words - v for k, v in gl_vocab_dic.items()}
+    unique_vocab_dic = {}
+    # uniq_v2 = v2 \cap v1' \cap v3' \cap v4' ... vn'
+    for k, v in gl_vocab_dic.items():
+        unique_vocab_dic[k] = v
+        for k2, v2 in complement_vocab_dic.items():
+            if k != k2:
+                unique_vocab_dic[k] &= v2  # do intersection and assignment
+
+    return unique_vocab_dic
+
+
+
+def data2freq(df, ngram, top_n, date_col_name='date', text_col_name='text'):
+    uniq_vocab_by_gl(df, text_col_name)
+    group = df.groupby([date_col_name])[text_col_name]
     # list of all the top words for every day
     counts = [dict(
         Counter(count_ngrams(g, ngram)).most_common(top_n)) for _, g in group]
@@ -178,6 +194,7 @@ def data2freq(df, ngram, top_n):
                 df_dic[k][-1] += top[k]
     df_dic['date'] = dates
     df = pd.DataFrame(df_dic)
+    # remove tuple notation
     df.rename(columns=lambda x: ' '.join(x) if x != 'date' else x, inplace=True)
     # prepare the new df
     return df
@@ -194,6 +211,7 @@ if __name__ == "__main__":
     nlp.add_argument('-ngram', default=False, type=pos_type)
     nlp.add_argument('-users', action='store_const', const='u', default=False)
     nlp.add_argument('-hashtags', action='store_const', const='h', default=False)
+    nlp.add_argument('-mutex', action='store_true', default=False)
     nlp.add_argument('-file')
     nlp.set_defaults(func=handle_nlp)
 
